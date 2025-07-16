ba9c02d6dc957559f90fec2ef72d23da598d1e84
mickeyLalata <mckyl1108@gmail.com>
2025-07-16 15:36:45 +0800

    added close form button

--- Project-Library/library-js.js
+++ Project-Library/library-js.js
@@ -2,3 +2,7 @@ function addBook() {
     document.getElementById("add-form").style.display = "block";
     document.getElementById("add-book").style.display = "none";
 }
+function closeForm() {
+    document.getElementById("add-form").style.display = "none";
+    document.getElementById("add-book").style.display = "block";
+}
