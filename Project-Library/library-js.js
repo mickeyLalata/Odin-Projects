c327cb2f6704c847bd568d5cab7d6207a961a51f
mickeyLalata <mckyl1108@gmail.com>
2025-07-16 15:50:49 +0800

    added function book

--- Project-Library/library-js.js
+++ Project-Library/library-js.js
@@ -1,3 +1,6 @@
+const cardContainer = document.querySelectorAll(".container");
+let myLibrary = [];
+
 function addBook() {
     document.getElementById("add-form").style.display = "block";
     document.getElementById("add-book").style.display = "none";
@@ -5,4 +8,12 @@ function addBook() {
 function closeForm() {
     document.getElementById("add-form").style.display = "none";
     document.getElementById("add-book").style.display = "block";
+}
+
+function book(title, author, pages, read) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
