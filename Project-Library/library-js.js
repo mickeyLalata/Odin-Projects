db052375f1b02eed319fd0c559223a0c6e936494
mickeyLalata <mckyl1108@gmail.com>
2025-07-17 19:17:35 +0800

    finalized library project

--- Project-Library/library-js.js
+++ Project-Library/library-js.js
@@ -1,19 +1,65 @@
-const cardContainer = document.querySelectorAll(".container");
+const cardContainer = document.querySelector(".container");
 let myLibrary = [];
 
-function addBook() {
+function AddBook() {
     document.getElementById("add-form").style.display = "block";
     document.getElementById("add-book").style.display = "none";
 }
-function closeForm() {
+function CloseForm() {
     document.getElementById("add-form").style.display = "none";
     document.getElementById("add-book").style.display = "block";
 }
 
-function book(title, author, pages, read) {
+function Book(title, author, pages) {
+    if (!new.target){
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+}
+
+function AddBookToLibrary () {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    
+    const addTitle = document.createElement("h1");
+    addTitle.textContent = "Title: " + newBook.title;
+    const addAuthor = document.createElement("p");
+    addAuthor.textContent = "Author: " + newBook.author;
+    const addPages = document.createElement("p");
+    addPages.textContent = "Pages: " + newBook.pages;
+    const read = document.createElement("label");
+    read.textContent = "Read: ";
+    const readCheckbox = document.createElement("input");
+    readCheckbox.type = "checkbox";
+    readCheckbox.checked = false
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.classList.add("delete-btn");
+
+    
+    deleteBtn.addEventListener("click", () => {
+    cardContainer.removeChild(bookCard);
+    });
+    
+    read.appendChild(readCheckbox)
+
+    bookCard.appendChild(addTitle);
+    bookCard.appendChild(addAuthor);
+    bookCard.appendChild(addPages);
+    bookCard.appendChild(read);
+    bookCard.appendChild(deleteBtn);
+
+
+    cardContainer.appendChild(bookCard);
 }
